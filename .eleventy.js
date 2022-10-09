2c41bb81110f8f426f4363a2c34e8352850e988f
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2022-10-09 16:28:45 -0400

    install pluggins and edit blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,36 @@
-const { DateTime } = require('luxon');
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/assets');
-  eleventyConfig.addPassthroughCopy('./src/style.css');
-  eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+  // https://www.11ty.dev/docs/plugins/syntaxhighlight/
+  eleventyConfig.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addFilter('postDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-      'LLLL dd, yyyy '
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "LLLL dd, yyyy "
     );
   });
 
+  // Set options for Markdown-it
+  let options = {
+    html: true, // Enable HTML tags in source
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    linkify: true, // Autoconvert URL-like text to links
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
+
   return {
     dir: {
-      input: 'src/',
-      output: 'public/',
+      input: "src/",
+      output: "public/",
     },
   };
 };
